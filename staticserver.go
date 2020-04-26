d57704eb7d915cb1f32a3aca3fca62376998aa46
derrick.zhu <derrick.zhu@farfetch.com>
2020-04-26 15:00:37 +0800

    update: fix some folder management

--- staticserver.go
+++ staticserver.go
@@ -14,30 +14,36 @@ import (
 
 var mux map[string]func(http.ResponseWriter, *http.Request)
 
-type Myhandler struct{}
-type home struct {
+type _FileServiceHandler struct{}
+type _Home struct {
 	Title string
 }
 
 const (
-	Template_Dir = "./view/"
-	Upload_Dir   = "./upload/"
+	strTemplateDir = "./view/"
+	strUploadDir   = "./upload/"
 )
 
 func main() {
+
+	if _, err := os.Stat(strUploadDir); os.IsNotExist(err) {
+		os.Mkdir(strUploadDir, 0777)
+		os.Chmod(strUploadDir, 0777)
+	}
+
 	server := http.Server{
 		Addr:        ":9090",
-		Handler:     &Myhandler{},
+		Handler:     &_FileServiceHandler{},
 		ReadTimeout: 10 * time.Second,
 	}
 	mux = make(map[string]func(http.ResponseWriter, *http.Request))
 	mux["/"] = index
 	mux["/upload"] = upload
-	mux["/file"] = StaticServer
+	mux["/file"] = _StaticServer
 	server.ListenAndServe()
 }
 
-func (*Myhandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (*_FileServiceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if h, ok := mux[r.URL.String()]; ok {
 		h(w, r)
 		return
@@ -53,7 +59,7 @@ func (*Myhandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func upload(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles(Template_Dir + "file.html")
+		t, _ := template.ParseFiles(strTemplateDir + "file.html")
 		t.Execute(w, "上传文件")
 	} else {
 		r.ParseMultipartForm(32 << 20)
@@ -68,24 +74,24 @@ func upload(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		filename := strconv.FormatInt(time.Now().Unix(), 10) + fileext
-		f, _ := os.OpenFile(Upload_Dir+filename, os.O_CREATE|os.O_WRONLY, 0660)
+		f, _ := os.OpenFile(strUploadDir+filename, os.O_CREATE|os.O_WRONLY, 0660)
 		_, err = io.Copy(f, file)
 		if err != nil {
 			fmt.Fprintf(w, "%v", "上传失败")
 			return
 		}
-		filedir, _ := filepath.Abs(Upload_Dir + filename)
+		filedir, _ := filepath.Abs(strUploadDir + filename)
 		fmt.Fprintf(w, "%v", filename+"上传完成,服务器地址:"+filedir)
 	}
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	title := home{Title: "首页"}
-	t, _ := template.ParseFiles(Template_Dir + "index.html")
+	title := _Home{Title: "首页"}
+	t, _ := template.ParseFiles(strTemplateDir + "index.html")
 	t.Execute(w, title)
 }
 
-func StaticServer(w http.ResponseWriter, r *http.Request) {
+func _StaticServer(w http.ResponseWriter, r *http.Request) {
 	http.StripPrefix("/file", http.FileServer(http.Dir("./upload/"))).ServeHTTP(w, r)
 }
 
